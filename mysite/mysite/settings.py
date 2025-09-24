764477f8b5b1401eb527b6d0a2bd0c6ba7d4919c
kiarash seyedmohamadi <seyedmohamadikiarash@gmail.com>
2025-09-24 12:14:30 +0330

    add main app for homepage

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.main',
     'apps.about',
     'apps.portfolio',
     'apps.blog',
