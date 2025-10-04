60ec3d06ed278e7f1d7f21de99dd58e1d32b7850
kiarash seyedmohamadi <seyedmohamadikiarash@gmail.com>
2025-10-04 13:11:21 +0330

    change database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  
-        'NAME': BASE_DIR / 'db.sqlite3',  
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbportfolio',
+        'USER': 'root',
+        'PASSWORD': '1370Aa@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 
